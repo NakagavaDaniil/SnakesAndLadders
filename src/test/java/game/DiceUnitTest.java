b965bc50a328ab16f2cf27ffdda10345d6d16cd6
Daniil Nakahava <daniil_nakahava@epam.com>
2021-01-11 20:26:44 +0200

    add DiceUnitTest.java test implementation

--- src/test/java/game/DiceUnitTest.java
+++ src/test/java/game/DiceUnitTest.java
@@ -1,7 +1,20 @@
 package game;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class DiceUnitTest {
 
+    private Dice dice1d6 = new Dice();
+
+    @Test
+    void rollShouldNotBeNull() {
+        assertThat(dice1d6.roll()).isNotNull();
+    }
+
+    @Test
+    void rollShouldReturnPositiveNumber() {
+        assertThat(dice1d6.roll()).isPositive();
+    }
 }
