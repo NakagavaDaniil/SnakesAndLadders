54f82a5d0fe39da8d019f80521f13cbe1335126f
Daniil Nakahava <daniil_nakahava@epam.com>
2021-01-11 21:56:54 +0200

    add Player.java implementation, test passes

--- src/main/java/game/Player.java
+++ src/main/java/game/Player.java
@@ -1,4 +1,21 @@
 package game;
 
 public class Player {
+
+    private static final int LAST_SQUARE_NUMBER = 100;
+    private int position = 1;
+
+    public int move(int diceResult) {
+        if (isPossiblePosition(diceResult)) {
+            position = position + diceResult;
+        }
+        return position;
+    }
+
+    private boolean isPossiblePosition(int diceResult) {
+        if (diceResult < 1 || diceResult > 6) {
+            throw new IllegalArgumentException();
+        }
+        return position + diceResult <= LAST_SQUARE_NUMBER;
+    }
 }
