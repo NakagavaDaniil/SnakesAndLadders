21549d1d15ecf6ee55f6f820c00f224cfce1df16
Daniil Nakahava <daniil_nakahava@epam.com>
2021-01-11 22:20:34 +0200

    setup SpringBoot , add test controller

---
+++ src/main/java/springboot/controller/GameController.java
@@ -0,0 +1,13 @@
+package springboot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GameController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
